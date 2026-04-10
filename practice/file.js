ca9adf6547cc38f8e67c834e61f8b53fb33edb9e
Addya Swaroop <addyaswaroop@gmail.com>
2026-04-10 12:34:56 +0530

    new

---
+++ practice/file.js
@@ -0,0 +1,27 @@
+let person={
+    name: "john",
+    age: 30,
+    city: "new york"
+};
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+
+let numbers=[1,2,3,4,5];
+console.log(number[0]);
+console.log(number[1]);
+console.log(number[2]);
+console.log(number[3]);
+console.log(number[4]);
+
+//mixed data types
+let arr=["hello",1,2,true,{name:" raju"}, [4,5,6]]
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+
+let newarray=[1,2,3,4,5];
+let sqarray= newarray.map(num=> num*num);
+console.log(sqarray)
